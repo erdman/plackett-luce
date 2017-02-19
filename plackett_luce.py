e0ec7586ccd91027c422f32d39dc53a50dbb3e77
Travis Erdman <travis.erdman@gmail.com>
2017-02-18 23:57:38 -0800

    Implement numpy version and speed improvements in native version. Fix bug from original implementation. Check convergence assumption and provide verbose output, if desired. Modify nascar test file to run both native and numpy versions.

--- plackett_luce.py
+++ plackett_luce.py
@@ -15,40 +15,138 @@ Source:  Sections 5 and 6 starting on p.395 (p.12 in pdf) of
 The algorithm here generates the BT factors using MLE.  A Bayesian approach has
 also recently been developed, which has some advantages.  See "Bayesian inference for
 Plackett-Luce ranking models"  (Guiver, Snelson; 2009).
-            
+
 '''
 
-import sqlite3
 from collections import Counter
-import argparse
+import sqlite3, argparse, time
+from math import sqrt
+from itertools import accumulate, combinations
+from graph_lib import scc
 
+try:
+    import numpy
+    HAVE_NUMPY = True
+except ImportError:
+    HAVE_NUMPY = False
 
 def retrieve_sqldata(filename, sqlstring):
     db = sqlite3.connect(filename)
     cursor = db.cursor()
     cursor.execute(sqlstring)
-    return cursor.fetchall()    
+    return cursor.fetchall()
 
-def plackett_luce(rankings):
-    ''' Returns dictionary containing player : plackett_luce_parameter keys
-    and values. This algorithm requires that every player avoids coming in
-    last place at least once and that every player fails to win at least once.
-    If this assumption fails (not checked), the algorithm will diverge.
+def plackett_luce(rankings, tolerance=1e-9, check_assumption=True, normalize=True, verbose=False):
+    '''This algorithm returns the MLE of the Plackett-Luce ranking parameters
+    over a given set of rankings.  It requires that the set of players is unable
+    to be split into two disjoint sets where nobody from set A has beaten anyone from
+    set B.  If this assumption fails, the algorithm will diverge.  If the
+    assumption is checked and fails, the algorithm will short-circuit and
+    return None.
 
     Input is a list of dictionaries, where each dictionary corresponds to an
     individual ranking and contains the player : finish for that ranking.
 
-    The plackett_luce parameters returned are un-normalized and can be
-    normalized by the calling function if desired.'''
+    Output is a dictionary containing player : plackett_luce_parameter keys
+    and values.
+    '''
+
     players = set(key for ranking in rankings for key in ranking.keys())
-    ws = Counter(name for ranking in rankings for name, finish in ranking.items() if finish < len(ranking))
+    rankings = [sorted(ranking.keys(),key=ranking.get) for ranking in rankings]
+    if check_assumption:
+        edges = [(source, dest) for ranking in rankings for source, dest in combinations(ranking, 2)]
+        scc_count = len(set(scc(edges).values()))
+        if verbose:
+            if scc_count == 1:
+                print ('No disjoint sets found.  Algorithm convergence conditions are met.')
+            else:
+                print('%d disjoint sets found.  Algorithm will diverge.'.format(scc_count))
+        if scc_count != 1:
+            return None
+
+    ws = Counter(name for ranking in rankings for name in ranking[:-1])
     gammas = {player : 1.0 / len(players) for player in players}
-    _gammas = {player : 0 for player in players}
-    while sum((gamma - _gammas[player]) ** 2 for player, gamma in gammas.items()) > 1e-9:
-        denoms = {player : sum(sum(0 if player not in ranking or ranking[player] < place else 1 / sum(gammas[finisher] for finisher in ranking if ranking[finisher] >= place) for place in range(1,len(ranking))) for ranking in rankings) for player in players}
+    gdiff = float('inf')
+    iteration = 0
+    start = time.perf_counter()
+    while gdiff > tolerance:
         _gammas = gammas
-        gammas = {player : ws[player] / denoms[player] for player in players}
+        gamma_sums   =  [[1 / s for s in reversed(list(accumulate(gammas[finisher] for finisher in reversed(ranking))))] for ranking in rankings]
+        gammas = {player : ws[player] / sum(sum(gamma_sum[:min(ranking.index(player) + 1, len(ranking) - 1)])
+                                            for ranking, gamma_sum in zip(rankings, gamma_sums)
+                                                if player in ranking)
+                                            for player in players}
+        if normalize:
+            gammas = {player : gamma / sum(gammas.values()) for player, gamma in gammas.items()}
+        pgdiff = gdiff
+        gdiff = sqrt(sum((gamma - _gammas[player]) ** 2 for player, gamma in gammas.items()))
+        iteration += 1
+        if verbose:
+            now = time.perf_counter()
+            print("%d %.2f seconds L2=%.2e" % (iteration, now-start, gdiff))
+            if gdiff > pgdiff:
+                print("Gamma difference increased, %.4e %.4e" % (gdiff, pgdiff))
+            start = now
     return gammas
+
+
+def pl_numpy(rankings, tolerance=1e-9, check_assumption=True, normalize=True, verbose=False):
+    """ Numpy implementation of the Plackett-Luce MLE algorithm described above.
+    Translated from the original Matlab code by Dr. David Hunter available at
+    http://sites.stat.psu.edu/~dhunter/code/btmatlab/plackmm.m
+    """
+    players = list(set(key for ranking in rankings for key in ranking.keys()))
+    rankings = [sorted(ranking.keys(),key=ranking.get) for ranking in rankings]
+    if check_assumption:
+        edges = [(source, dest) for ranking in rankings for source, dest in combinations(ranking, 2)]
+        scc_count = len(set(scc(edges).values()))
+        if verbose:
+            if scc_count == 1:
+                print ('No disjoint sets found.  Algorithm convergence conditions are met.')
+            else:
+                print('%d disjoint sets found.  Algorithm will diverge.'.format(scc_count))
+        if scc_count != 1:
+            return None
+
+    ws = Counter(name for ranking in rankings for name in ranking[:-1])
+
+    # matlab code is 1-based, we're using 0-based so be wary of off-by-ones
+    a = numpy.array([(players.index(name) + 1, ranking_index, finish) for ranking_index, ranking in enumerate(rankings, 1) for finish, name in enumerate(ranking,1)], dtype = int)
+    M, N, P = numpy.max(a, axis=0)   #finding the counts of players and contests and the max rank ... I would have used len, but following orignal code
+    f = numpy.zeros((P, N), dtype=int)
+    r = numpy.zeros((M, N), dtype=int)
+    f[a[:,2] - 1, a[:,1] - 1] = a[:,0]
+    r[a[:,0] - 1, a[:,1] - 1] = a[:,2] + P * (a[:,1] - 1)
+
+    w = numpy.array([ws[player] for player in players], dtype=int)
+    pp = sum(f > 0)  # players per contest
+
+    gammas = numpy.ones((M)) / M
+    gdiff = float('inf')
+    iterations = 0
+    start = time.perf_counter()
+    while gdiff > tolerance:
+        iterations += 1
+        g = (f > 0).choose(0, gammas[f - 1].squeeze())
+        g = numpy.cumsum(g[::-1,:],axis=0)[::-1,:]   #reverse vertical cumsum
+        g[pp - 1, numpy.arange(numpy.shape(g)[1])] = 0
+        g[g > 0] = 1 / g[g > 0]
+        numpy.cumsum(g,axis=0,out=g)
+        r2 = (r > 0).choose(0, g.T.flat[r - 1])  #array indexing like Matlab https://stackoverflow.com/questions/20688881/numpy-assignment-and-indexing-as-matlab
+        _gammas = gammas
+        gammas = w / numpy.sum(r2,axis=1)
+        if normalize:
+            gammas /= numpy.sum(gammas)
+        pgdiff = gdiff
+        gdiff = numpy.linalg.norm(gammas - _gammas)
+        if verbose:
+            now = time.perf_counter()
+            print("%d %.2f seconds L2=%.2e" % (iterations, now-start, gdiff))
+            if gdiff > pgdiff:
+                print("Gamma difference increased, %.4e %.4e" % (gdiff, pgdiff))
+            start = now
+
+    return {player : gamma for player, gamma in zip(players, gammas)}
 
 
 if __name__ == '__main__':
--- test_pl_nascar.py
+++ test_pl_nascar.py
@@ -7,13 +7,96 @@ with open('nascar2002.txt','r') as infile:
     rankings = [tuple(map(int,line.split())) for line in infile.readlines()[1:]]    #drop header
 
 rankings = [{driver:place for driver, race, place in group} for _, group in groupby(rankings,key=lambda x: x[1])]
-gammas = plackett_luce(rankings)
-normalizing_constant = sum(gammas.values())
-gammas = {driver : value / normalizing_constant for driver, value in gammas.items()}
-
+gammas = plackett_luce(rankings, verbose=True)
 for gamma, player in sorted([(v,k) for k,v in gammas.items()], reverse=True):
     print("{:<5}{:6.4f}".format(player, gamma))
 
+from plackett_luce import pl_numpy as plackett_luce
+gammas = plackett_luce(rankings, verbose=True)
+for gamma, player in sorted([(v,k) for k,v in gammas.items()], reverse=True):
+    print("{:<5}{:6.4f}".format(player, gamma))
 
-
-    
+#~ travis@F555L ~/Documents/halite/plackett-luce $ ./test_pl_nascar.py
+#~ 58   0.1864
+#~ 68   0.1096
+#~ 54   0.0274
+#~ 51   0.0235
+#~ 66   0.0230
+#~ 37   0.0205
+#~ 82   0.0184
+#~ 32   0.0168
+#~ 72   0.0167
+#~ 61   0.0161
+#~ 31   0.0154
+#~ 48   0.0153
+#~ 52   0.0148
+#~ 13   0.0146
+#~ 63   0.0146
+#~ 80   0.0142
+#~ 12   0.0138
+#~ 2    0.0135
+#~ 67   0.0133
+#~ 14   0.0127
+#~ 60   0.0126
+#~ 64   0.0126
+#~ 53   0.0123
+#~ 33   0.0118
+#~ 62   0.0115
+#~ 4    0.0115
+#~ 76   0.0112
+#~ 49   0.0109
+#~ 77   0.0102
+#~ 27   0.0099
+#~ 42   0.0099
+#~ 38   0.0097
+#~ 45   0.0094
+#~ 44   0.0093
+#~ 34   0.0092
+#~ 3    0.0090
+#~ 21   0.0084
+#~ 18   0.0082
+#~ 36   0.0081
+#~ 50   0.0080
+#~ 41   0.0076
+#~ 74   0.0073
+#~ 55   0.0073
+#~ 43   0.0072
+#~ 35   0.0070
+#~ 25   0.0070
+#~ 22   0.0068
+#~ 10   0.0065
+#~ 7    0.0065
+#~ 5    0.0063
+#~ 26   0.0062
+#~ 28   0.0062
+#~ 6    0.0062
+#~ 73   0.0061
+#~ 9    0.0061
+#~ 83   0.0059
+#~ 79   0.0058
+#~ 59   0.0057
+#~ 39   0.0057
+#~ 78   0.0055
+#~ 23   0.0053
+#~ 71   0.0053
+#~ 65   0.0052
+#~ 20   0.0052
+#~ 56   0.0049
+#~ 16   0.0039
+#~ 15   0.0030
+#~ 1    0.0029
+#~ 70   0.0029
+#~ 69   0.0028
+#~ 40   0.0025
+#~ 19   0.0022
+#~ 81   0.0022
+#~ 17   0.0022
+#~ 8    0.0021
+#~ 47   0.0021
+#~ 11   0.0019
+#~ 57   0.0019
+#~ 46   0.0019
+#~ 30   0.0017
+#~ 29   0.0017
+#~ 75   0.0015
+#~ 24   0.0014
